f284f1f5f87b9f27007c1122a3f2485653201ca2
Power Zhang <PZhang@walmartlabs.com>
2019-06-27 14:32:21 -0700

    test update

--- src/test/java/blog/brucefeng/info/TestMVEL.java
+++ src/test/java/blog/brucefeng/info/TestMVEL.java
@@ -17,17 +17,15 @@ public class TestMVEL {
 
   public static void main(String[] args) {
 
-    final String expression = "{ 'firstName', 'middleName', 'lastName' } contains SOMESTRING";
+    final String expression = "{'cfe43191-1a81-38ef-9f3c-245bff7d37ac','6135fe8c-1ba9-4360-beab-e53691b020ad-wrong'} contains SOMESTRING";
 
     final Map<String, String> params = new HashMap<String, String>();
-    params.put( "SOMESTRING", "firstName" );
+    params.put( "SOMESTRING", "cfe43191-1a81-38ef-9f3c-245bff7d37ac" );
     System.out.println( MVEL.eval( expression, params ) );
 
-    params.put( "SOMESTRING", "first" );
+    params.put( "SOMESTRING", "6135fe8c-1ba9-4360-beab-e53691b020ad" );
     System.out.println( MVEL.eval( expression, params ) );
 
-    params.put( "SOMESTRING", "address" );
-    System.out.println( MVEL.eval( expression, params ) );
   }
 
 }
